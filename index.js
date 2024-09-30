a6fb97fbc2be3b7940e9ceeed6d0abd41886fd84
unknown <amasoali0@gmail.com>
2024-09-30 16:14:14 +0530

    port no

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT
 const app = express()
 app.use(cors({
     credentials:true,
-    origin:'http://localhost:5173'
+    origin:'https://notes-server-two.vercel.app/'
 }))
 
 
